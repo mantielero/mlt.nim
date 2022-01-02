433f5697244f4842067d7a073de4a52f34d256b4
mantielero <josemaria.alkala@gmail.com>
2022-01-02 10:03:49 +0100

    Animation example added

--- examples/ex25_affine_anim.nim
+++ examples/ex25_affine_anim.nim
@@ -15,16 +15,28 @@ proc main =
   var clip1 = p.newMedia( "./resources/sygic.mp4")
 
   var aff = newFactoryFilter(p, "affine")   
-  aff["transition.ox"] = "0=0;100=200"
-  #aff["transition.rotate_x"] = "0=0;100=360"
+  #aff["transition.ox"] = "0=0;100=200"
+  #aff["transition.rotate_x"] = 2.0
+  aff["transition.keyed"] = 1
+  aff["transition.rotate_y"] = "1= 0;100= 90"
+  #aff["transition.rect"] = "0=0%/0%:100%x100%:100%;100=100%/0%:100%x100%:100%"
 
   clip1.attach(aff)
 
   clip1 > sdl
 
-  sdl.start # Start the consumer
+  sdl.run
+  #sdl.start # Start the consumer
 
-  while not sdl.stopped:
-    sleep(1)
+  #while not sdl.stopped:
+  #  sleep(1)
 
 main()
+
+#[
+$ melt -proifle hdv_720_50p -consumer sdl2 +hello.txt -attach affine transition.rotate_x="1=1.5;100=200"
+
+$ melt -proifle hdv_720_50p -consumer sdl2 +hello.txt -attach affine transition.rotate_y=4
+
+$ melt -profile hdv_720_50p -consumer sdl2 +hello.txt -attach affine transition.keyed=1 transition.rotate_x="0=1;100=10"
+]#
