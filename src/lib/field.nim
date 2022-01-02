8d89530c8cebaaa70f0c31ad88cb8b42c4b65b8c
mantielero <josemaria.alkala@gmail.com>
2022-01-02 14:20:32 +0100

    plant for both transtion and filter

--- src/lib/field.nim
+++ src/lib/field.nim
@@ -1,11 +1,29 @@
+##[
+The field is a convenience class that works with the tractor and multitrack classes to manage track filters and transitions.
+]##
 import ../wrapper/mlt
 import typs, os, std/strformat
 
 
-proc plantTransition*(self: Field; that: Transition; a_track, b_track: int) =
+proc plant*(self: Field; that: Transition; a_track, b_track: int) =
+  ##[
+  Plant a transition.
+  ]##
   let res = mlt_field_plant_transition(self.data, that.data, a_track.cint, b_track.cint)
   if res > 0:
     quit("""unable to plant transition""")
+
+proc plant*(self: Field; that: Filter; track: int) =
+  ##[
+  Plant a filter.
+  ]##
+  let res = mlt_field_plant_filter(self.data, that.data, track.cint)
+  if res > 0:
+    quit("""unable to plant filter""")
+
+
+proc close*(self: Field) =
+  mlt_field_close(self.data)
 
 #[
 proc mlt_field_init*(): mlt_field {.importc, cdecl, impmltDyn.}
@@ -17,12 +35,11 @@ proc mlt_field_tractor*(self: mlt_field): mlt_tractor {.importc, cdecl,
     impmltDyn.}
 proc mlt_field_multitrack*(self: mlt_field): mlt_multitrack {.importc, cdecl,
     impmltDyn.}
+
 proc mlt_field_properties*(self: mlt_field): mlt_properties {.importc, cdecl,
     impmltDyn.}
-proc mlt_field_plant_filter*(self: mlt_field; that: mlt_filter; track: cint): cint {.
-    importc, cdecl, impmltDyn.}
 
-proc mlt_field_close*(self: mlt_field) {.importc, cdecl, impmltDyn.}
+
 proc mlt_field_disconnect_service*(self: mlt_field; service: mlt_service) {.
     importc, cdecl, impmltDyn.}
 ]#
