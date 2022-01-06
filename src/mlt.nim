4e7e39d1c6dad8ab2026ca360261067e42ad4ebd
mantielero <josemaria.alkala@gmail.com>
2022-01-06 21:30:25 +0100

    Added references to contrib folder

--- src/mlt.nim
+++ src/mlt.nim
@@ -1,8 +1,10 @@
 import wrapper/mlt
 import lib/[version, factory, profile, typs, consumer, producer, filter, properties, service, playlist, transition, tractor]
-import lib/[field, multitrack, events, repository]
+import lib/[field, multitrack, events, repository, frame]
+import contrib/[stacking, mirrored]
 
 
 export mlt
 export version, factory, profile, typs, consumer, producer, filter, properties, service, playlist, transition, tractor
-export field, multitrack, events, repository
+export field, multitrack, events, repository, frame
+export stacking, mirrored
